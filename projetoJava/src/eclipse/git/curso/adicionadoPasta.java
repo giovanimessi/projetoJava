4c2f3320430af1966a046ac53eb6b57cace08047
Giovani Moura Messias <giovani.messias@el.com.br>
2022-10-23 12:37:17 -0300

    adicionado atributo idade na classe

--- projetoJava/src/eclipse/git/curso/adicionadoPasta.java
+++ projetoJava/src/eclipse/git/curso/adicionadoPasta.java
@@ -4,10 +4,20 @@ public class adicionadoPasta {
 	
 	private String nome;
 	private String sobrenome;
+	private String idade;
+	
 	public String getNome() {
 		return nome;
 	}
 	
+	public String getIdade() {
+		return idade;
+	}
+
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
