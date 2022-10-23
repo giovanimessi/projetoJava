bf368f79021e1a2bd52de2036b6e6983c7b4bcbb
Giovani Moura Messias <giovani.messias@el.com.br>
2022-10-23 10:24:26 -0300

    criacao do projeto

---
+++ projetoJava/src/projetoJava/Ola.java
@@ -0,0 +1,15 @@
+package projetoJava;
+
+public class Ola {
+
+	
+	public static void main (String[]args) {
+		
+		System.out.print("*"+'\n');
+		System.out.print("***" + '\n');
+		System.out.print("***");
+
+
+		
+	}
+}
